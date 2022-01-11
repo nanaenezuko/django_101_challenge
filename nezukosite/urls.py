a40796c5a60dbe4c712a083b2cc705211e82add5
Nanae <bllsbrthr@gmail.com>
2022-01-11 08:59:38 +0700

    setting up django template

--- nezukosite/urls.py
+++ nezukosite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+
+from photouploader import urls as photouploader_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(photouploader_urls, namespace='photouploader'))
 ]
